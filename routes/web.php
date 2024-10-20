bf30f1224d5345967c29a2a3cf11a94844d6bf81
amany <eng.am.aboelella@gmail.com>
2024-10-20 07:24:18 +0300

    update web routs

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,38 @@
 <?php
 
+use App\Models\User;
+use App\Models\Ticket;
 use Illuminate\Support\Facades\Route;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use App\Http\Controllers\RoadMap\DataController;
+use App\Http\Controllers\Auth\OidcAuthController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Share ticket
+Route::get('/tickets/share/{ticket:code}', function (Ticket $ticket) {
+    return redirect()->to(route('filament.resources.tickets.view', $ticket));
+})->name('filament.resources.tickets.share');
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+// Validate an account
+Route::get('/validate-account/{user:creation_token}', function (User $user) {
+    return view('validate-account', compact('user'));
+})
+    ->name('validate-account')
+    ->middleware([
+        'web',
+        DispatchServingFilamentEvent::class
+    ]);
 
+// Login default redirection
+Route::redirect('/login-redirect', '/login')->name('login');
+
+// Road map JSON data
+Route::get('road-map/data/{project}', [DataController::class, 'data'])
+    ->middleware(['verified', 'auth'])
+    ->name('road-map.data');
+
+Route::name('oidc.')
+    ->prefix('oidc')
+    ->group(function () {
+        Route::get('redirect', [OidcAuthController::class, 'redirect'])->name('redirect');
+        Route::get('callback', [OidcAuthController::class, 'callback'])->name('callback');
+    });
