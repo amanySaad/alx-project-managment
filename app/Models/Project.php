57faf888812bd43fca4ee19447826060921c3955
amany <eng.am.aboelella@gmail.com>
2024-10-20 07:16:30 +0300

    add activity , epic and sprint db models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -17,7 +17,8 @@ class Project extends Model implements HasMedia
     use HasFactory, SoftDeletes, InteractsWithMedia;
 
     protected $fillable = [
-        'name', 'description', 'status_id', 'owner_id'
+        'name', 'description', 'status_id', 'owner_id', 'ticket_prefix',
+        'status_type', 'type'
     ];
 
     protected $appends = [
@@ -44,13 +45,91 @@ class Project extends Model implements HasMedia
         return $this->hasMany(Ticket::class, 'project_id', 'id');
     }
 
+    public function statuses(): HasMany
+    {
+        return $this->hasMany(TicketStatus::class, 'project_id', 'id');
+    }
+
+    public function epics(): HasMany
+    {
+        return $this->hasMany(Epic::class, 'project_id', 'id');
+    }
+
+    public function sprints(): HasMany
+    {
+        return $this->hasMany(Sprint::class, 'project_id', 'id');
+    }
+
+    public function epicsFirstDate(): Attribute
+    {
+        return new Attribute(
+            get: function () {
+                $firstEpic = $this->epics()->orderBy('starts_at')->first();
+                if ($firstEpic) {
+                    return $firstEpic->starts_at;
+                }
+                return now();
+            }
+        );
+    }
+
+    public function epicsLastDate(): Attribute
+    {
+        return new Attribute(
+            get: function () {
+                $firstEpic = $this->epics()->orderBy('ends_at', 'desc')->first();
+                if ($firstEpic) {
+                    return $firstEpic->ends_at;
+                }
+                return now();
+            }
+        );
+    }
+
+    public function contributors(): Attribute
+    {
+        return new Attribute(
+            get: function () {
+                $users = $this->users;
+                $users->push($this->owner);
+                return $users->unique('id');
+            }
+        );
+    }
+
     public function cover(): Attribute
     {
         return new Attribute(
-            get: fn() =>
-                $this->media('cover')?->first()?->getFullUrl()
+            get: fn() => $this->media('cover')?->first()?->getFullUrl()
                 ??
                 'https://ui-avatars.com/api/?background=3f84f3&color=ffffff&name=' . $this->name
+        );
+    }
+
+    public function currentSprint(): Attribute
+    {
+        return new Attribute(
+            get: fn() => $this->sprints()
+                ->whereNotNull('started_at')
+                ->whereNull('ended_at')
+                ->first()
+        );
+    }
+
+    public function nextSprint(): Attribute
+    {
+        return new Attribute(
+            get: function () {
+                if ($this->currentSprint) {
+                    return $this->sprints()
+                        ->whereNull('started_at')
+                        ->whereNull('ended_at')
+                        ->where('starts_at', '>=', $this->currentSprint->ends_at)
+                        ->orderBy('starts_at')
+                        ->first();
+                }
+                return null;
+            }
         );
     }
 }
