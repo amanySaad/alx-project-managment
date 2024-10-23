cf2d573cf59e737b5c261ad3f7f23babedcb37ef
Amany Saad <eng.am.aboelella@gmail.com>
2024-10-23 19:10:25 +0300

    update seeder file for basic auth credentials

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'Arafat Hossain',
-            'email' => 'admin@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@alx-project.com',
             'password' => bcrypt('secret'),
         ]);
     }
